5c329cd3bb71709445306f43e69d1849ac6227a7
Chris <chrismoneil99@gmail.com>
2022-10-29 12:26:15 -0400

    Started to read in data

--- data_munging.py
+++ data_munging.py
@@ -2,3 +2,17 @@
 Christopher O'Neil
 Script that does preliminary data cleaning and analysis
 """
+
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def read_data(query: str, database: str) -> pd.DataFrame:
+    conn = sqlite3.connect(database)
+    df = pd.read_sql_query(query, conn)
+    return df
+
+
+choice_data = read_data("SELECT * FROM CHOICE WHERE YEAR==2010", "TFS_CHOICE_2008_2010.db")
+demo_data = read_data("SELECT * FROM DEMOGRAPHICS WHERE Surveyyear == 2010", "DEMOGRAPHICS.db")
