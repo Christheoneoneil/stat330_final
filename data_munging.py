1d90d99fba27018751d2a26f11df6b5833e3a953
Chris <chrismoneil99@gmail.com>
2022-10-30 11:55:36 -0400

    Initial cleaning, merging of data, dataset now contains 95,697 rows, instead of initial 200,000

--- data_munging.py
+++ data_munging.py
@@ -6,6 +6,7 @@ Script that does preliminary data cleaning and analysis
 import sqlite3
 import pandas as pd
 import os
+from functools import reduce
 
 
 def read_data(query: str, database: str, csv_name) -> pd.DataFrame:
@@ -17,6 +18,7 @@ def read_data(query: str, database: str, csv_name) -> pd.DataFrame:
         conn = sqlite3.connect(database)
         df = pd.read_sql_query(query, conn)
         df.dropna(axis=1, how="all", inplace=True)
+
         df.to_csv(csv_name)
         return df
 
@@ -24,9 +26,22 @@ def read_data(query: str, database: str, csv_name) -> pd.DataFrame:
     return df
 
 
-def engineer_data(data: pd.DataFrame):
+def merge_data(df_list: list, id_var_dict)-> pd.DataFrame:
+    merged_df = reduce(lambda x, y: pd.merge(x, y, left_on=id_var_dict[str(x)], right_on=id_var_dict[str(y)]), df_list)
+    merged_df.drop(columns=["Unnamed: 0_x", "Unnamed: 0_y"] + list(id_var_dict.values())[1:], inplace=True)
+    return merged_df
+
+
+def explore_data(data: pd.DataFrame):
     data_copy = data.copy()
+    na_vals = data_copy.isna().sum().sort_values(ascending=False) / len(data_copy) * 100
+    print(na_vals)
+    data_copy.dropna(axis=0, how="any", inplace=True)
+    print(data_copy)
 
 
 choice_data = read_data("SELECT * FROM CHOICE WHERE YEAR==2010", "TFS_CHOICE_2008_2010.db", "choice.csv")
 demo_data = read_data("SELECT * FROM DEMOGRAPHICS WHERE Surveyyear == 2010", "DEMOGRAPHICS.db", "demo.csv")
+merged_data = merge_data([choice_data, demo_data], id_var_dict={str(choice_data):"SUBJID", str(demo_data):"SubjectI.D."})
+explore_data(merged_data)
+
