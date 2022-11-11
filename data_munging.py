f1f2a7f7fcbfb91af0656da172517fa2d52f146a
Chris <chrismoneil99@gmail.com>
2022-11-11 13:41:18 -0500

    Imputing columns as to not lose large amounts of data.

--- data_munging.py
+++ data_munging.py
@@ -7,7 +7,6 @@ import sqlite3
 import pandas as pd
 import os
 from functools import reduce
-import zipcodes
 
 
 def read_data(query: str, database: str, csv_name) -> pd.DataFrame:
@@ -39,7 +38,7 @@ def merge_data(df_list: list, merge_var) -> pd.DataFrame:
     merge data merges the given data frames into one to
     prepare for data engineering.
     :param df_list: list of data frames
-    :param id_var_dict: dictionary to keep track of columns to merge
+    :param merge_var: variable to merge on
     :return: merged pandas data frame
     """
     merged_df = reduce(lambda x, y: pd.merge(x, y, on=merge_var, how='outer'), df_list)
@@ -59,11 +58,14 @@ def data_engineering(data: pd.DataFrame, desired_stats: list, unwanted_cols: lis
     """
 
     from sklearn.preprocessing import LabelEncoder
+    from sklearn.experimental import enable_iterative_imputer
+    from sklearn.impute import IterativeImputer
     data_copy = data.copy()
-    print(list(data_copy.columns))
     na_vals = data_copy.isna().sum().sort_values(ascending=False) / len(data_copy) * 100
     print(na_vals)
-
+    imputed_cols = ["SATVerbal", "SATMath", "SATWriting", "ACTComposite"]
+    data_copy[imputed_cols] = IterativeImputer(max_iter=10, sample_posterior=True).fit_transform(data_copy[imputed_cols])
+    data_copy.drop(columns=unwanted_cols, inplace=True)
     data_copy.dropna(axis=0, how="any", inplace=True)
 
     desc_df = data_copy.describe()
@@ -72,24 +74,23 @@ def data_engineering(data: pd.DataFrame, desired_stats: list, unwanted_cols: lis
 
     str_cols = list(data_copy.select_dtypes(include=object).columns)
     data_copy[str_cols] = data_copy[str_cols].apply(LabelEncoder().fit_transform)
-    data_copy["is_real_zip"] = [zipcodes.is_real(str(int(zip_code))) for zip_code in data_copy["Studentshomezip"]]
-    print(data_copy[data_copy["is_real_zip"] == False])
-    data_copy.drop(unwanted_cols, axis=1, inplace=True)
-    data_copy.to_csv("final_frame.csv")
 
+
+    data_copy.to_csv("final_frame.csv")
+    print(data_copy)
 
 merging_var = "SubjectI.D."
 choice_data = read_data("SELECT * FROM CHOICE WHERE YEAR==2010", "TFS_CHOICE_2008_2010.db", "choice.csv")
 choice_data.rename(columns={"SUBJID": merging_var}, inplace=True)
 demo_data = read_data("SELECT * FROM DEMOGRAPHICS WHERE Surveyyear == 2010", "DEMOGRAPHICS.db", "demo.csv")
-high_data = read_data("SELECT [SubjectI.D.], SATVerbal, SATMath, SATWriting ACTComposite FROM 'HIGH SCHOOL' WHERE Surveyyear == 2010",
+high_data = read_data("SELECT [SubjectI.D.], SATVerbal, SATMath, SATWriting, ACTComposite FROM 'HIGH SCHOOL' WHERE Surveyyear == 2010",
                       "HIGH SCHOOL.db", "high_school.csv")
 
 merged_data = merge_data([choice_data, demo_data, high_data], merging_var)
-print(merged_data)
+
 unneeded_cols = ["NORMSTAT", "STUDSTAT", "YEAR", "Surveyyear", "Studentshomezip", "AmericanIndian/AlaskaNative",
                  "NativeHawaiian/PacificIslander", "AfricanAmerican/Black", "MexicanAmerican/Chicano/o/x", "PuertoRican",
-                  "OtherLatino/o/x", "White/Caucasian", "Other", "Asian", "RecodedCollegeI.D.", "is_real_zip",
+                  "OtherLatino/o/x", "White/Caucasian", "Other", "Asian", "RecodedCollegeI.D.",
                   "SurveyType", "Areyouenrolled(orenrolling)asa:", "Seximputed", "InstitutionControl", "InstitutionType"]
 
 data_engineering(merged_data, ["min", "max", "std"], unneeded_cols)
